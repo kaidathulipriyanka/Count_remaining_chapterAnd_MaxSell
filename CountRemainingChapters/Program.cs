1864bc37adb962e0026957ea2e37f0986160078f
kaidathulipriyanka <131505554+kaidathulipriyanka@users.noreply.github.com>
2023-10-11 22:37:38 +0530

    [priya]: CountRemainingChapters And Maximumprice

---
+++ CountRemainingChapters/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CountRemainingChapters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.Write("Enter the number of chapters (N): ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<Tuple<int, int>> chapters = new List<Tuple<int, int>>();
+
+            Console.WriteLine("Enter the start and end pages of each chapter:");
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.Write($"Chapter {i + 1} - Start Page: ");
+                int startPage = int.Parse(Console.ReadLine());
+
+                Console.Write($"Chapter {i + 1} - End Page: ");
+                int endPage = int.Parse(Console.ReadLine());
+
+                chapters.Add(Tuple.Create(startPage, endPage));
+            }
+
+            Console.Write("Enter the current reading page (K): ");
+            int K = int.Parse(Console.ReadLine());
+
+            int remainingChapters = CalculateRemainingChapters(chapters, K);
+
+            Console.WriteLine("Number of chapters yet to read: " + remainingChapters);
+        }
+
+        static int CalculateRemainingChapters(List<Tuple<int, int>> chapters, int K)
+        {
+            int remainingChapters = 0;
+
+            foreach (var chapter in chapters)
+            {
+                int startPage = chapter.Item1;
+                int endPage = chapter.Item2;
+
+                 if (startPage <= K && K <= endPage)
+                {
+                     remainingChapters = chapters.Count - chapters.IndexOf(chapter);
+                    break;
+                }
+            }
+
+            return remainingChapters;
+        }
+    }
+}
---
+++ MaximumSellingPrice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace MaximumSellingPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                string[] firstLine = Console.ReadLine().Split();
+                int n = int.Parse(firstLine[0]);
+                int k = int.Parse(firstLine[1]);
+
+                List<int> basketA = Console.ReadLine().Split().Select(int.Parse).ToList();
+                List<int> basketB = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+                List<int> sortedBasketB = new List<int>(basketB);
+                sortedBasketB.Sort();
+
+                for (int i = 0; i < n; i++)
+                {
+                    if (k > 0 && basketA[i] < sortedBasketB[n - 1 - i])
+                    {
+                        basketA[i] = sortedBasketB[n - 1 - i];
+                        k--;
+                    }
+                }
+
+                int maxPrice = basketA.Sum();
+                Console.WriteLine(maxPrice);
+            }
+        }
+
+    }
+}
